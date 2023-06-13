31a90d4dbd211025e1d6d4e01222aa499b2a1159
alexandre-mo <alexandre.moreau2001@gmail.com>
2023-06-13 21:57:08 +0200

    Amélioration des vagues du header et footer

--- projetelective/src/App.vue
+++ projetelective/src/App.vue
@@ -20,27 +20,30 @@
           src="./assets/Logo outlined 64x64.png"
         />
       </div>
-    </v-app-bar>
-
-    <svg class="wave-top" viewBox="0 0 1440 50">
+      <svg class="wave-top" viewBox="0 0 1440 50">
       <path 
         fill="#DA7635" 
         fill-opacity="1" 
         d="M 0 25 Q 360 -25 720 25 Q 1080 75 1440 25 L 1440 0 L 0 0 L 0 50"
       />
     </svg>
+    </v-app-bar>
+
+   
 
     <v-main>
       <router-view/>
     </v-main>
 
-    <svg class="wave-bottom" viewBox="0 0 1440 50">
-      <path 
-        fill="#DA7635" 
-        fill-opacity="1" 
-        d="M 0 25 Q 360 -25 720 25 Q 1080 75 1440 25 L 1440 50 L 0 50 L 0 50"
-      />
-    </svg>
+    <div class="footer" style="position: relative;">
+      
+
+      <v-footer app style="background-color: rgb(255,0,0,0);">
+        <svg class="wave-bottom" viewBox="0 0 1440 50" style="position: absolute; bottom: 100%; margin-bottom: -50px;">
+        <path fill="#DA7635" fill-opacity="1" d="M 0 25 Q 360 -25 720 25 Q 1080 75 1440 25 L 1440 50 L 0 50 L 0 50" />
+      </svg>
+      </v-footer>
+    </div>
   </v-app>
 </template>
 
@@ -105,6 +108,13 @@ footer {
   z-index: 20;
   background-color: #DA7635;
   /* Add your other styles for the footer here */
+}
+
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
 }
 
 .wave-top {

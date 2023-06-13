cfabde435cbb839301a64b0ef85a2d0d2f09e90a
SnipeDragons <73822060+SnipeDragons@users.noreply.github.com>
2023-06-13 16:29:52 +0200

    finished adding vuetify

--- projetelective/src/App.vue
+++ projetelective/src/App.vue
@@ -1,12 +1,27 @@
 <template>
-  <div>
-    <!-- Header -->
-    <header>
-      <!-- Add your navigation links here -->
-      <h1>Something</h1>
-    </header>
+  <v-app>
+    <v-app-bar
+      app
+      dark
+      color="#DA7635"
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Roll Eat Logo"
+          class="d-none d-sm-flex"
+          contain
+          src="./assets/Logo outlined 128x64.png"
+        />
 
-    <!--vague supérieure-->
+        <v-img
+          alt="Roll Eat Logo"
+          class="d-flex d-sm-none"
+          contain
+          src="./assets/Logo outlined 64x64.png"
+        />
+      </div>
+    </v-app-bar>
+
     <svg class="wave-top" viewBox="0 0 1440 50">
       <path 
         fill="#DA7635" 
@@ -15,9 +30,10 @@
       />
     </svg>
 
-    <router-view/>
+    <v-main>
+      <router-view/>
+    </v-main>
 
-    <!--vague inférieure-->
     <svg class="wave-bottom" viewBox="0 0 1440 50">
       <path 
         fill="#DA7635" 
@@ -25,150 +41,28 @@
         d="M 0 25 Q 360 -25 720 25 Q 1080 75 1440 25 L 1440 50 L 0 50 L 0 50"
       />
     </svg>
-
-    <!-- Footer -->
-    <footer>
-      <!-- Add your credits here -->
-      <h1>Something</h1>
-    </footer>
-  </div>
+  </v-app>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
 
-
-
-<!-- rest of your App.vue file -->
-
-<script>
-
-export default {
+export default Vue.extend({
   name: 'App',
-  components: {
-  }
-}
+
+  data: () => ({
+    //
+  }),
+});
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;800&display=swap');
-
-* {
-  font-family: 'Poppins', sans-serif;
-  margin:0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-#app {
-  max-width: 100%;
-}
-
-body {
-  background-image: linear-gradient(62deg, #DB995A 0%, #D6D4A0 100%);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  padding:32px;
-}
-
-img {
-  max-width: 100%;
-  border-radius: 8px;
-}
-
-
-header {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: 100px; /* Height of the header */
-  z-index: 20;
-  background-color: #DA7635;
-  /* Add your other styles for the header here */
-}
-
-footer {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 100px; /* Height of the footer */
-  z-index: 20;
-  background-color: #DA7635;
-  /* Add your other styles for the footer here */
-}
-
 .wave-top {
   position: absolute;
-  top: 100px;
+  top: 64px;
   left: 0;
   right: 0;
   z-index: 10;
   width: 100%;
 }
-
-.wave-bottom {
-  position: absolute;
-  bottom: 100px; /* Height of the footer */
-  left: 0;
-  right: 0;
-  z-index: 10;
-  width: 100%;
-}
-
-.card {
-  max-width: 100%;
-  width: 540px;
-  background:white;
-  border-radius: 16px;
-  padding:32px;
-}
-
-.card__title {
-  text-align:center;
-  font-weight: 800;
-}
-
-.card__subtitle {
-  text-align: center;
-  color:#666;
-  font-weight: 500;
-}
-
-  .button {
-    background: #2196F3;
-    color:white;
-    border-radius: 8px;
-    font-weight: 800;
-    font-size: 15px;
-    border: none;
-    width: 100%;
-    padding: 16px;
-    transition: .4s background-color;
-  }
-
-  .card__action {
-    color:#2196F3;
-    text-decoration: underline;
-  }
-
-  .card__action:hover {
-    cursor:pointer;
-  }
-
-  .button:hover {
-    cursor:pointer;
-    background: #1976D2;
-  }
-
-  .button--disabled {
-    background:#cecece;
-    color:#ececec
-  }
-  .button--disabled:hover {
-    cursor:not-allowed;
-    background:#cecece;
-  }
-
 </style>

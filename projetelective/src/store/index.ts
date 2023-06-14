6d0e857b1ef98a3ddb410cb0d7697ff5c24251ee
antoniorosemary <antoniorosemary@hotmail.fr>
2023-06-14 10:26:23 +0200

    auth MAJ 1.0

--- projetelective/src/store/index.ts
+++ projetelective/src/store/index.ts
@@ -1,22 +1,101 @@
 import { createStore } from 'vuex'
 
-export default createStore({
+const axios = require('axios');
+
+const instance = axios.create({
+  baseURL: 'https://www.wawasensei.dev/api/demo-auth/'
+});
+
+let user = localStorage.getItem('user');
+if (!user) {
+ user = {
+    userId: -1,
+    token: '',
+  }; 
+} else {
+  try {
+    user = JSON.parse(user);
+    instance.defaults.headers.common['Authorization'] = user.token;
+  } catch (ex) {
+    user = {
+      userId: -1,
+      token: '',
+    };
+  }
+}
+
+// Create a new store instance.
+const store = createStore({
   state: {
-    user: null
+    status: '',
+    user: user,
+    userInfos: {
+      nom:'',
+      prenom: '',
+      email: '',
+      photo: '',
+    },
   },
   mutations: {
-    setUser(state, user) {
-      state.user = user
+    setStatus: function (state, status) {
+      state.status = status;
+    },
+    logUser: function (state, user) {
+      instance.defaults.headers.common['Authorization'] = user.token;
+      localStorage.setItem('user', JSON.stringify(user));
+      state.user = user;
+    },
+    userInfos: function (state, userInfos) {
+      state.userInfos = userInfos;
+    },
+    logout: function (state) {
+      state.user = {
+        userId: -1,
+        token: '',
+      }
+      localStorage.removeItem('user');
     }
   },
   actions: {
-    login({ commit }, user) {
-      // TODO: Authenticate the user with your API and get the user data
-      commit('setUser', user)
+    login: ({commit}, userInfos) => {
+      commit('setStatus', 'loading');
+      return new Promise((resolve, reject) => {
+        instance.post('/login', userInfos)
+        .then(function (response) {
+          commit('setStatus', '');
+          commit('logUser', response.data);
+          resolve(response);
+        })
+        .catch(function (error) {
+          commit('setStatus', 'error_login');
+          reject(error);
+        });
+      });
     },
-    logout({ commit }) {
-      // TODO: Log out the user from your API
-      commit('setUser', null)
+    createAccount: ({commit}, userInfos) => {
+      commit('setStatus', 'loading');
+      return new Promise((resolve, reject) => {
+        commit;
+        instance.post('/createAccount', userInfos)
+        .then(function (response) {
+          commit('setStatus', 'created');
+          resolve(response);
+        })
+        .catch(function (error) {
+          commit('setStatus', 'error_create');
+          reject(error);
+        });
+      });
+    },
+    getUserInfos: ({commit}) => {
+      instance.post('/infos')
+      .then(function (response) {
+        commit('userInfos', response.data.infos);
+      })
+      .catch(function () {
+      });
     }
   }
 })
+
+export default store;

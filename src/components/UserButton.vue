4ae690af94073d8484583cc2b1e22b4b9b68c495
SnipeDragons <73822060+SnipeDragons@users.noreply.github.com>
2023-06-15 11:29:40 +0200

    added routing

--- src/components/UserButton.vue
+++ src/components/UserButton.vue
@@ -1,17 +1,34 @@
 <template>
   <div>
-    <v-btn solid color="success">
+    <v-btn v-if="loggedOut" solid color="success" @click="GoToLogin">
       <v-icon left>
         mdi-account-arrow-down-outline
       </v-icon>
       Connexion
+    </v-btn>
+    <v-btn v-else solid color="succes">
+      <v-icon v-if="userProfilePictureB64 == undefined" left>
+        mdi-account
+      </v-icon>
+      <v-img v-else left :src="'data:image/png;base64, '+userProfilePictureB64"/>
+      {{userName}}
     </v-btn>
   </div>
 </template>
   
 <script lang="ts">
 export default {
-  components: {}
+  components: {},
+  data: () => {return {
+    loggedOut: true,
+    userName: "jean valjean",
+    userProfilePictureB64: undefined
+  }},
+  methods:{
+    GoToLogin() {
+      window.location.href = /*process.env.BASE_URL*/"http://localhost:8080"+"/login"
+    }
+  }
 }
 </script>
 
